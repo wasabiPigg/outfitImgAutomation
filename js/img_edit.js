ae45ad17b917fefb147774e5a376d255b1c5328d
wasabiPigg <wasabiPigg@gmail.com>
2020-12-31 12:49:41 +0900

    hoge

--- js/img_edit.js
+++ js/img_edit.js
@@ -1,57 +1,63 @@
-/*
-jquery-imgedit
-by mehdibo
-https://github.com/mehdibo/jquery-imgedit
-*/
+var file = document.getElementById('file');
+var canvas = document.getElementById('canvas');
+var canvasWidth = 900;
+var canvasHeight = 900;
+var uploadImgSrc;
 
-(function($) {
-	$.fn.imgEdit = function(options) {
-		// Default settings
-		var settings = $.extend({
-			inputName : "profile-picture",
-			form : "#profile",
-		}, options);
+// Canvasの準備
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+var ctx = canvas.getContext('2d');
 
-		$(this).click(function(e){
-			
-			var target = this;
-			// Create the file input
-			input = document.createElement('input');
-			// Add attributes
-			input.setAttribute('type', 'file');
-			input.setAttribute('name', settings.inputName);
-			input.setAttribute('accept', '.jpg, .jpeg, .png');
-			input.setAttribute('style', 'display:none;');
-			// Trigger a click
-			input.click();
-			// Wait for change
-			input.onchange = function() {
-				// Remove any previous input
-				$("#imgEditInput").remove();
-				// Set ID to the new input
-				input.setAttribute('id', 'imgEditInput');
-				// Read file if any
-				if(input.files && input.files[0]){
-					var reader = new FileReader();
-					reader.onload = function(e) {
-				    	$(target).attr('src', e.target.result);
-				    }
-					reader.readAsDataURL(input.files[0]);
-					
-					// Apend input
-					$(settings.form).append(input);
-				}
-				
-				
-			};
-		});
+function loadLocalImage(e) {
+  // ファイル情報を取得
+  var fileData = e.target.files[0];
 
-	};
+  // 画像ファイル以外は処理を止める
+  if(!fileData.type.match('image.*')) {
+    alert('画像を選択してください');
+    return;
+  }
 
-}(jQuery));
+  // FileReaderオブジェクトを使ってファイル読み込み
+  var reader = new FileReader();
+  // ファイル読み込みに成功したときの処理
+  reader.onload = function() {
+    // Canvas上に表示する
+    uploadImgSrc = reader.result;
+    canvasDraw();
+  }
+  // ファイル読み込みを実行
+  reader.readAsDataURL(fileData);
+}
 
+// ファイルが指定された時にloadLocalImage()を実行
+file.addEventListener('change', loadLocalImage, false);
 
-$("#target").imgEdit({
-			inputName : "profile-picture",
-			form : "#target-form",
-		});
+// Canvas上に画像を表示する
+function canvasDraw() {
+  // canvas内の要素をクリアする
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  // Canvas上に画像を表示
+  var img = new Image();
+  img.src = uploadImgSrc;
+  img.onload = function() {
+    ctx.drawImage(img, 0, 0, canvasWidth, this.height * (canvasWidth / this.width));
+
+    // Canvas上にテキストを表示
+    addText();
+  }
+}
+
+// Canvas上にテキストを表示する
+function addText() {
+  ctx.fillStyle = '#fdd000';
+  ctx.fillRect(10, 10, 140, 30);
+
+  ctx.font = "bold 20px 'MS Pゴシック'";
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = '#002B69';
+  ctx.fillText('株式会社TAM', 80, 25);
+}
