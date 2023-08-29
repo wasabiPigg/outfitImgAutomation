0fb2357f6bd6e71b8fb8f7ec635da74e7c3012fc
wasabiPigg <wasabiPigg@gmail.com>
2023-08-30 02:37:32 +0900

    一個だけ長方形を検出できるようになった

--- js/opencv_main.js
+++ js/opencv_main.js
@@ -10,12 +10,13 @@ function threshold() {
     let mat = cv.imread(imgElement);
     var dst = new cv.Mat();
     cv.cvtColor(mat, dst, cv.COLOR_RGBA2GRAY, 0);
+    cv.imshow('canvasOutputGrayScale', dst);
 
 
     let threshold = new cv.Mat();
     // 白黒反転して2値化する
     cv.threshold(dst, threshold, 230, 255, cv.THRESH_BINARY_INV)
-    // cv.imshow('canvasOutput', threshold);
+    cv.imshow('canvasOutputThreshold', threshold);
 
     findContours(threshold);
     mat.delete();
@@ -23,38 +24,43 @@ function threshold() {
 }
 
 /// 輪郭を抽出する
-function findContours(dst) {
+function findContours(threshold) {
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
-    cv.findContours(dst, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+    cv.findContours(threshold, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
     
-    aa(contours, hierarchy, dst);
+    aa(contours, hierarchy, threshold);
     hierarchy.delete();
     contours.delete();
 }
 
 /// 輪郭の矩形領域
-function aa(contours, hierarchy, img) {
+function aa(contours, hierarchy, threshold) {
     // 画像の読み込み
     let readImg = cv.imread(imgElement);
+    let readImg2 = cv.imread(imgElement);
 
     var itemNum = 0;
-    for (const i in contours) {
-      itemNum++;
-      console.log(itemNum);
-      console.log(i);
-    }
+    // for (const i in contours) {
+    //   console.log(i);
+    //   itemNum++;
+    //   console.log(itemNum);
+    // }
 
+    console.log(contours);
     var rect = cv.boundingRect(contours.get(0));
-    let contoursColor = new cv.Scalar(0, 255, 0);
-    let rectangleColor = new cv.Scalar(255, 0, 0);
-    cv.drawContours(readImg, contours, 0, contoursColor, 1, 8, hierarchy, 100);
+    let contoursColor = new cv.Scalar(0, 255, 0, 255);
+    let rectangleColor = new cv.Scalar(255, 0, 0, 255);
+    cv.drawContours(readImg, contours, -1, contoursColor, 5, 8, hierarchy, 100);
+    cv.drawContours(readImg2, contours, -1, contoursColor, 5, 8, hierarchy, 100);
+    cv.imshow('canvasOutputFindContours', readImg);
 
 
     let point1 = new cv.Point(rect.x, rect.y);
     let point2 = new cv.Point(rect.x + rect.width, rect.y + rect.height);
-    cv.rectangle(img, point1, point2, rectangleColor, 2, cv.LINE_AA, 2);
-    cv.imshow('canvasOutput', img);
+    console.log(rect);
+    cv.rectangle(readImg2, point1, point2, rectangleColor, 5, cv.LINE_AA);
+    cv.imshow('canvasOutput', readImg2);
     
 }
 
