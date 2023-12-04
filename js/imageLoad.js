578d8fda5941b5dc13507c116901964bea184df9
wasabiPigg <wasabiPigg@gmail.com>
2023-12-05 02:10:51 +0900

    いらないデータを削除

--- js/imageLoad.js
+++
@@ -1,7 +0,0 @@
-// 黒猫さんのテンプレ用のチェキフレーム画像を事前に読み込んでおく
-const kuronekoBg01 = new Image();
-kuronekoBg01.src = "https://github.com/wasabiPigg/outfitImgAutomation/blob/master/img/testImg001.png?raw=true" // 仮のデータ
-
-// 黒猫さんのテンプレ用の空っぽのアイテムリスト画像を事前読み込み
-const nullItemImg = new Image();
-nullItemImg.src = "https://github.com/wasabiPigg/outfitImgAutomation/blob/master/img/nullItemList.png?raw=true"
